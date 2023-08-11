ed97fbb66bd03bc1748bc31b51165d128d704f71
Maor Arie <maorarye10@gmail.com>
2023-08-12 02:24:34 +0300

    Dev update 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import '../styles/style.css'
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/utils.css";
