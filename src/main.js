b8a2e69a094f9c515db22c8e54d7e098071d179f
Maor Arie <maorarye10@gmail.com>
2023-08-15 01:25:35 +0300

    about + featured sections

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/featured.css";
 import "../styles/utils.css";
